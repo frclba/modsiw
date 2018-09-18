059de2b77bcd6c17abded263361f078b9a5e48c5
Fernando Aguilar <fernando@aguilar.net.br>
2018-09-18 20:04:53 -0300

    Evolving Deep Dream with helper funcs

--- LPFDS/DeepDream.py
+++ LPFDS/DeepDream.py
@@ -19,7 +19,7 @@ def main():
         model_url = urllib.request.urlopen(url)
         with open(local_zip_file, 'wb') as output:
             output.write(model_url.read())
-        
+
         #Extract
         with zipfile.ZipFile(local_zip_file, 'r') as zip_ref:
             zip_ref.extractall(data_dir)
@@ -41,11 +41,97 @@ def main():
     t_preprocessed = tf.expand_dims(t_input-imagenet_mean, 0)
     tf.import_graph_def(graph_def, {'input':t_preprocessed})
 
-    layers = [op.name for op in grapth.get_operations() if op.type == 'Conv2D' and 'import/' in op.name]
-    features_num = [int(grapth.get_tensor_by_name(name+':0').get_shape()[-1]) for name in layers]
+    layers = [op.name for op in graph.get_operations() if op.type == 'Conv2D' and 'import/' in op.name]
+    features_num = [int(graph.get_tensor_by_name(name+':0').get_shape()[-1]) for name in layers]
 
-    print('Number of layers', len(layers))
-    print('Total number of feature channels:', sum(features_num))
+    print('Number of layers ', len(layers))
+    print('Total number of feature channels: ', sum(features_num))
+#End main
+
+    # Helper functions for TF Graph visualization
+    def strip_consts(graph_def, max_const_size=32):
+        strip_def = tf.GraphDef()
+        for n0 in graph_def.node:
+            n = strip_def.node.add() #pylint: disable=maybe-no-member
+            n.MergeFrom(n0)
+            if n.op == 'Const':
+                tensor = n.attr['value'].tensor
+                size = len(tensor.tensor_content)
+                if size > max_const_size:
+                    tensor.tensor_content = "<stripped %d bytes>" % size
+            return strip_def
+    #End strip_consts
+
+    def rename_nodes(graph_def, rename_func):
+        res_def = tf.GraphDef()
+        for n0 in graph_def.node:
+            n = res_def.node.add()
+            n.MergeFrom(n0)
+            n.name = rename_func(n.name)
+            for i, s in enumerate(n.input):
+                n.input[i] = rename_func(s) if s[0] != '^' else '^'+rename_func(s[1:])
+        return res_def
+    #End rename_nodes
+
+    def showarray(a):
+        a = np.uint8(np.clip(a, 0, 1) * 255)
+        plt.imshow(a)
+        plt.show()
+    #End showarray
+
+    def visstd(a, s=0.1):
+        return (a-a.mean()) / max(a.std(), 1e-4) * s + 0.5
+    #End visstd
+
+    def T(layer):
+        return graph.get_tensor_by_name("import/%s:0" % layer)
+    #End T
+
+    def render_naive(t_obj, img0=img_noise, iter_n=20, step=1.0):
+        t_score = tf.reduce_mean(t_obj)  # defining the optimization objective
+        t_grad =  tf.gradients(t_score, t_input)[0]
+
+        img = img0.copy()
+        for _ in range(iter_n):
+            g, _ = sess.run([t_grad, t_score], {t_input:img})
+            g /= g.std() + 1e-8
+            img += g * step
+        showarray(visstd(img))
+    #End render_naive
+
+    def tffunc(*argtypes):
+        '''Helper that transforms TF-graph generating function into a regular one.
+            See "resize" function below.
+        '''
+        placeholders = list(map(tf.placeholder, argtypes))
+        def wrap(f):
+            out = f(*placeholders)
+            def wrapper(*args, **kw):
+                return out.eval(dict(zip(placeholders, args)), session=kw.get('session'))
+            return wrapper
+        return wrap
+
+    def resize(img, size):
+        img = tf.expand_dims(img, 0)
+        return tf.image.resize_bilinear(img, size)[0,:,:,:]
+    resize = tffunc(np.float32, np.int32)(resize)
+
+    def calc_grad_tiled(img, t_grad, tile_size=512):
+        '''Compute the value of tensor t_grad over the image in a tiled way.
+        Random shifts are applied to the image to blur tile boundaries over 
+        multiple iterations.'''
+        sz = tile_size
+        h, w = img.shape[:2]
+        sx, sy = np.random.randint(sz, size=2)
+        img_shift = np.roll(np.roll(img, sx, 1), sy, 0)
+        grad = np.zeros_like(img)
+        for y in range(0, max(h-sz//2, sz),sz):
+            for x in range(0, max(w-sz//2, sz),sz):
+                sub = img_shift[y:y+sz,x:x+sz]
+                g = sess.run(t_grad, {t_input:sub})
+                grad[y:y+sz,x:x+sz] = g
+    return np.roll(np.roll(grad, -sx, 1), -sy, 0) 
+
 
 if __name__ == '__main__':
     main()
